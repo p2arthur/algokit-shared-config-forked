618983014d8efda6c6db928fd5ce2015f7254426
Larkin Young <larkin.young@algorand.foundation>
2025-12-17 15:57:22 +0000

    Reverted back to single typedoc config file

--- .github/actions/generate-typescript-docs/generate-typedoc-config.js
+++ .github/actions/generate-typescript-docs/generate-typedoc-config.js
@@ -4,37 +4,24 @@ const path = require("path");
 const actionDir = __dirname;
 const workspaceDir = process.cwd();
 
-const htmlConfigPath = path.join(actionDir, "typedoc.html.json");
-const mdConfigPath = path.join(actionDir, "typedoc.markdown.json");
-const finalHtmlConfigPath = path.join(workspaceDir, "typedoc.html.json");
-const finalMarkdownConfigPath = path.join(
-  workspaceDir,
-  "typedoc.markdown.json"
-);
+const configPath = path.join(actionDir, "typedoc.base.json");
+const finalConfigPath = path.join(workspaceDir, "typedoc.base.json");
 
 try {
-  if (
-    fs.existsSync(finalHtmlConfigPath) &&
-    fs.existsSync(finalMarkdownConfigPath)
-  ) {
-    console.log("ℹ️  User provided config files found.");
+  if (fs.existsSync(finalConfigPath)) {
+    console.log("ℹ️  User provided config file found.");
     console.log("   Skipping dynamic generation to respect project settings.");
     process.exit(0);
   }
-  if (!fs.existsSync(htmlConfigPath)) {
-    console.error(`HTML Docs config not found at: ${htmlConfigPath}`);
+  if (!fs.existsSync(configPath)) {
+    console.error(`Docs config not found at: ${configPath}`);
     process.exit(1);
   }
-  if (!fs.existsSync(mdConfigPath)) {
-    console.error(`Markdown Docs config not found at: ${mdConfigPath}`);
-    process.exit(1);
-  }
-  const htmlConfig = require(htmlConfigPath);
-  const mdConfig = require(mdConfigPath);
 
-  fs.writeFileSync(finalHtmlConfigPath, JSON.stringify(htmlConfig, null, 2));
-  fs.writeFileSync(finalMarkdownConfigPath, JSON.stringify(mdConfig, null, 2));
-  console.log(`✅ Generated config files at: ${finalConfigPath}`);
+  const config = require(configPath);
+
+  fs.writeFileSync(finalConfigPath, JSON.stringify(config, null, 2));
+  console.log(`✅ Generated config file at: ${finalConfigPath}`);
 } catch (error) {
   console.error("❌ Error generating config:", error);
   process.exit(1);
